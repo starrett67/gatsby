87accc60085b4843cf610c81ad38d302fdfce974
Joshua Starrett <jstarrett@roomstogo.com>
2020-05-06 08:40:54 -0400

    a change

--- packages/gatsby/src/commands/build.ts
+++ packages/gatsby/src/commands/build.ts
@@ -116,7 +116,6 @@ module.exports = async function build(program: IBuildArgs): Promise<void> {
       type: `SET_WEBPACK_COMPILATION_HASH`,
       payload: webpackCompilationHash,
     })
-
     activity = report.activityTimer(`Rewriting compilation hashes`, {
       parentSpan: buildSpan,
     })
@@ -171,8 +170,8 @@ module.exports = async function build(program: IBuildArgs): Promise<void> {
   // Rebuild subset of pages if user opt into GATSBY_EXPERIMENTAL_PAGE_BUILD_ON_DATA_CHANGES
   // if there were no source files (for example components, static queries, etc) changes since last build, otherwise rebuild all pages
   const currentWebpackCompilationHash = store.getState().webpackCompilationHash
-  console.log("CURRENT WEBPACK HASH", currentWebpackCompilationHash)
-  console.log("CASHED WEBPACK HASH", cachedWebpackCompilationHash)
+  console.log(`CURRENT WEBPACK HASH`, currentWebpackCompilationHash)
+  console.log(`CASHED WEBPACK HASH`, cachedWebpackCompilationHash)
   if (
     process.env.GATSBY_EXPERIMENTAL_PAGE_BUILD_ON_DATA_CHANGES &&
     cachedWebpackCompilationHash === currentWebpackCompilationHash
